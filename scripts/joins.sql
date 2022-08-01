79f09eb5b25462de87bed7fba694da42c4d17e21
Matthew Lilley <mklilley@users.noreply.github.com>
2022-08-01 20:07:24 +0100

    Update joins.sql

--- scripts/joins.sql
+++ scripts/joins.sql
@@ -7,6 +7,7 @@ CREATE TABLE adoptions (
 
 INSERT INTO adoptions (cat_id, owner_id) VALUES (1, 1), (3, 2);
 
+-- Get the name of owners and the cats they've adopted
 SELECT owners.name AS owner, cats.name AS cat
 FROM ((adoptions JOIN owners ON adoptions.owner_id = owners.id)
 JOIN cats ON adoptions.cat_id = cats.id);
